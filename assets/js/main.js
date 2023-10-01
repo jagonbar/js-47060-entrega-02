582afb867b29a86a276f0b7e50e2a20bec2f741e
javier gonzalez <jgonzalezb@indap.cl>
2023-10-01 17:36:41 -0300

    funciones validacion, venta y crea html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,57 +1,189 @@
+//--------------------------------------------------------------------------------
+// globales
+
+//div para mostrar tablas
 const consolajs = document.getElementById("consolajs");
-let venta = []
-let html = "";
-let td  = "<td>valor</td>"
-let tr  = "<tr>valor</tr>"
-let img = "<img src=\"ruta\" alt=\"cover metalgear\">"
-productos.forEach((p)=>{
-    let html_producto
-    html_producto += td.replace("valor",p.titulo);
-    html_producto += td.replace("valor",p.precio);
-    html_producto += td.replace("valor",p.descripcion);    
-    html_producto += td.replace("valor",(img.replace("ruta",p.imagen)))
-    html += tr.replace("valor",html_producto);
-});
 
-consolajs.innerHTML = html;
+//elementos para crear tablas
 
+const html ={
+    "td"   : "<td extra_atributos>valor</td>",
+    "tr"   : "<tr extra_atributos>valor</tr>",
+    "p"   : "<p extra_atributos>valor</p>",
+    "img"  : "<img src=\"valor\" extra_atributos/>",
+}
+
+//guarda productos a vender
+const venta = []
+
+//muestra tabla catálogo de productos
+muestraTablaProducto();
+
+//espera la carga de catálogo y empieza la venta
 setTimeout(() => {
     
         realizaVenta();
-    }, 3000);
+    }, 4000);
+//--------------------------------------------------------------------------------
+/**
+ * crea tabla catálogo de productos
+ */
+function muestraTablaProducto(){
+    
+    let html = creaHTML("tr",
+        creaHTML("td","TITULO")+
+        creaHTML("td","PRECIO")+
+        creaHTML("td","DESCRIPCION")+
+        creaHTML("td","IMAGEN")
+    );
+    productos.forEach((p)=>{
+        let html_producto;
+        html_producto += creaHTML("td",p.titulo);
+        html_producto += creaHTML("td",p.precio);
+        html_producto += creaHTML("td",p.descripcion);    
+        html_producto += creaHTML("img",p.imagen,[{"alt":"imagen juego"}]);
+        html += creaHTML("tr",html_producto);
+    });
+    html = creaHTML("table",html,[{"border":"1"}]);
+    consolajs.innerHTML = html;
+}
+//--------------------------------------------------------------------------------
+/**
+ * crea elemento html
+ * @param {string} tag html
+ * @param {string} valor 
+ * @param {string|array<json>} extra_atributos [{atributo:valor},...]
+ * @returns 
+ */
+function creaHTML(tag, valor, extra_atributos=""){
+    let texto_extra_atributos="";
+    let elemento = html[tag].replace("valor",valor);
+    
+    if(Array.isArray(extra_atributos)){
+        extra_atributos.forEach(atributo=>{
+            texto_extra_atributos+= `"${atributo.id}"="${atributo.valor}" `;
+        })
+    }else{
+        texto_extra_atributos=extra_atributos
+    }
+    elemento = elemento.replace("extra_atributos",texto_extra_atributos)
+    return elemento;
+}
+//--------------------------------------------------------------------------------
+/**
+ * pide producto y agrega detalle a venta
+ */
 function realizaVenta(){
     variable = "un producto";
     while(preguntaOpcionSIoNO(`¿Quieres comprar ${variable} ?\nResponde:\nSI para comprar\nNO para finalizar compra`)){
         
-        pedirProducto();
+        let {id,cantidad} = pedirProductoaVender();
         
+        agregarDetalleVenta(id, cantidad);
         
         variable = "otro producto más";
     }
     finalizaVenta()
 }
+//--------------------------------------------------------------------------------
+/**
+ * crea tabla resumen de venta en pantalla
+ */
 function finalizaVenta(){
 
-}
+    let total_venta=0;
+    //calcular total de venta
+    total_venta =venta.reduce((total_venta, producto)=>total_venta+producto.total, 0);
+    //crear html tabla resumen venta
+    let html = creaHTML("tr",
+        creaHTML("td","TITULO")+
+        creaHTML("td","PRECIO")+
+        creaHTML("td","CANTIDAD")+
+        creaHTML("td","TOTAL")
+    );
 
-function pedirProducto(){        
+    venta.forEach((p)=>{
+        let html_producto;
+        html_producto += creaHTML("td",p.titulo);        
+        html_producto += creaHTML("td",p.precio);
+        html_producto += creaHTML("td",p.cantidad);
+        html_producto += creaHTML("td",p.total);
+        html += creaHTML("tr",html_producto);
+    });
+    html = creaHTML("tr",
+        creaHTML("td","TOTAL",[{"colspan":3}])+
+        creaHTML("td",total_venta)        
+    );
+    html = creaHTML("table",html,[{"border":"1"}]);
+    consolajs.innerHTML = html; 
+}
+//--------------------------------------------------------------------------------
+/**
+ * pedir de producto y cantidad a vender.
+ * finalmente se agrega detalle a venta
+ */
+function pedirProductoaVender(){        
         let id = pedirIdyRevisar();            
                 
         let cantidad = pedirCantidad();
-
-        let producto_a_comprar = productos.find((p)=>id==p.id);
-         
-        agregarDetalleVenta(producto_a_comprar, cantidad);    
+        return {id,cantidad}                         
 }
-function revisaSiExisteEnVenta(id){
-    //aca
-    let se_encuentra = venta.find((p)=>{return p.id==id});
-    if(se_encuentra){
+//--------------------------------------------------------------------------------
+/**
+ * pedir id de producto de la lista existente
+ * sigue preguntando por id si:
+ * - id no existe en lista de producto
+ * - se encontró que producto ya está en la venta.
+ *  - en este caso se pregunta si se quiere reemplazar
+ * @returns id int
+ */
+function pedirIdyRevisar(){
+    
+    
+    let se_encuentra_producto;
+    let se_encuentra_en_venta;
 
-    }
+    let sigue_preguntando=true
+    do{
+        
+        //---------------------------------
+        /* Pedir ID de producto*/
+        let msg = `Ingresar id de producto:`
+        let id = prompt(msg);
+        
+        //Revisar que se encuentre producto        
+        se_encuentra_producto= (productos.indexOf(id)!=-1)
+
+        //si no se encuentra cortar ciclo y pedir ID otra vez
+        if(!se_encuentra_producto){
+            msg=`Debe ingresar un id de la lista  de productos`;
+            alert(msg)
+            continue;
+        }
+        
+        //revisar si se encuentra ya el producto pedido en la venta
+        // si está preguntar si se reemplaza o pedir otro id
+        se_encuentra_en_venta= (venta.indexOf(id) != -1);
+        if(se_encuentra_en_venta){
+            msg=`El producto ya se encuentra agregado a la venta\n
+            ¿Quieres reemplazar el producto?`;
+            if(preguntaOpcionSIoNO(msg)){
+                sacarProductoDeVenta(id);
+            }
+            continue;
+        }
+        sigue_preguntando=false;
+    }while(sigue_preguntando)
+    return id;
 }
-
-function agregarDetalleVenta(producto,cantidad){
+//--------------------------------------------------------------------------------
+/**
+ * Agregar producto con detalle de cantidad a vender con total
+ * @param {Int} producto 
+ * @param {Int} cantidad 
+ */
+function agregarDetalleVenta(id_producto,cantidad){
+    let producto = productos.find((p)=>{p.id==id_producto});
     venta.push({
           "id": producto.id
         , "titulo": producto.titulo
@@ -60,61 +192,84 @@ function agregarDetalleVenta(producto,cantidad){
         , "total" : cantidad*producto.precio
     });
 }
-
-function pedirId(){
-    do{
-        let id    = prompt("Indica id de producto a comprar");
-        es_valido = validaNumero(id) && validaIdExista(id);
-    }while(!es_valido)
-
-    let id = revisaSiExisteEnVenta(id);
-    
-    return id
-}
+//--------------------------------------------------------------------------------
+/**
+ * pide cantidad a vender entre 1 a 100
+ * si numero está fuera de rango, se sigue preguntando
+ * @returns cantidad
+ */
 function pedirCantidad(){
     do{
-        let cantidad=prompt("Indica cantidad a comprar (de 1 a 100) ");
-        es_valido   = validaNumero(cantidad) &&validaRango(cantidad);
+        let cantidad= prompt("Indica cantidad a comprar (de 1 a 100) ");
+        es_valido   = validaNumeroInt(cantidad) && validaRango(cantidad);
     }while(!es_valido)
-    return id
+    return cantidad;
 }
-function validaIdExista(id){    
-    let se_encuentra = (!productos.indexOf(id))
-    if(!se_encuentra) alert(`El id ${id} no tiene producto asociado`);
-    return se_encuentra;
-}
-function validaRango(cantidad){    
-    let es_rango_valido = (cantidad>0 && cantidad<101);
+//--------------------------------------------------------------------------------
+/**
+ * retorna verdadero o falso si es que cantidad esta entre 1 y 100 o no.
+ * @param {int} cantidad 
+ * @returns {boolean}
+ */
+function validaRango(cantidad){
+    let [minimo,maximo] = [1,100];
+    let es_rango_valido = (minimo>=0 && cantidad<=maximo);
     if(!es_rango_valido) alert("Cantidad debe ser un rango entre 1 y 100");
-    return es_rango_valido;
-}
-function sumaProducto(id,cantidad,precio){
-    let p = productos.filter((p)=>id==p.id);
-    let producto_a_vender = {
-        "id": p[0].id,
-        cantidad,
-        precio,
-        "total_producto":cantidad*precio
-    };
-    venta.push(producto_a_vender);        
-}
-
+    return es_rango_valido;}
+//--------------------------------------------------------------------------------
 function preguntaOpcionSIoNO(texto_pregunta){
     let validacion = true;
     do{
         let respuesta_continuar_compra = prompt(texto_pregunta);
-        validacion = validaOpcion(respuesta_continuar_compra,["SI","NO"]);
+        validacion = validaOpcion(respuesta_continuar_compra,[{"opcion":"SI","valor":true},{"opcion":"NO","valor":false}]);
     }while(validacion);
     
     return (respuesta_continuar_compra=="SI");
 }
+//--------------------------------------------------------------------------------
+function validaOpcion(respuesta_dada,opciones_validas){
+    es_opcion_valida = (opciones_validas.indexOf(respuesta_dada)!=-1);
+    
+    let msg = `Opción no válida debe ingresar:\n${opciones_validas.join(", ")}`
 
-prompt("Indica id de producto a comprar")
-    //no encuentra
-    "indica un id de la tabla.\nEl id id no tiene producto asociado"
-    pregunta_reemplazar = prompt("el producto ya se encuentra agregado,¿Quieres reemplazarlo?")
-    //si    
-    prompt("cantidad")
-    alert("cantidad:c | precio:p | total:t\n\nMonto total venta hasta el momento:venta")
-    //no
-continuar_compra = prompt("¿Quieres comprar otro?\nResponde:\nSI para comprar\nNO para finalizar compra");
+    if(!es_opcion_valida) alert(msg);
+    return es_opcion_valida
+}
+//--------------------------------------------------------------------------------
+/**
+ * saca producto de la venta, dejandolo al final para eliminarlo con pop
+ * @param {*} id 
+ */
+function sacarProductoDeVenta(id){
+    //buscar id de producto
+    //dejarlo al final
+    //sacarlo con pop
+    venta.sort((producto)=>{
+        return (producto.id==id)?1:0;            
+    });
+    venta.pop()
+}
+//--------------------------------------------------------------------------------
+/**
+ * valida si es un número entero válido
+ * @param {string} valor 
+ * @returns {boolean}
+ */
+function validaNumeroInt(valor){
+    es_valido = Number.isInteger(valor);
+    if(!es_valido) alert("Ingresa un número valido");
+    return es_valido;
+}
+
+
+//--------------------------------------------------------------------------------
+
+// prompt("Indica id de producto a comprar")
+//     //no encuentra
+//     "indica un id de la tabla.\nEl id id no tiene producto asociado"
+//     pregunta_reemplazar = prompt("el producto ya se encuentra agregado,¿Quieres reemplazarlo?")
+//     //si    
+//     prompt("cantidad")
+//     alert("cantidad:c | precio:p | total:t\n\nMonto total venta hasta el momento:venta")
+//     //no
+// continuar_compra = prompt("¿Quieres comprar otro?\nResponde:\nSI para comprar\nNO para finalizar compra");
